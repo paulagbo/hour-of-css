114b4fcdaf6af39daba35c9d4353192c9eb5c35f
paulagbo <paul-david.agbo@acity.edu.gh>
2019-01-26 11:50:16 +0000

    Added javascript to index

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload= function(e) {
+    alert('Today is the first day of the rest of your life');
+    
+    var barnabas = document.getElementById('developer');
+    console.log(barnabas.innerText);
+    barnabas.innerText = 'YO';
+};
